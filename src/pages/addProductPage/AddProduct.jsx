df108fa669fdf1d3d6d8b83016be4f7e70a8d09b
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2024-02-04 00:05:40 +0600

    product add system added

--- src/pages/addProductPage/AddProduct.jsx
+++ src/pages/addProductPage/AddProduct.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiImageOn } from "react-icons/ci";
 
 const AddProduct = () => {
+  const [img, setImg] = useState([]);
+
+  const handleImgSubmit = (e) => {
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    console.log(file);
+
+    if (file) {
+      reader.onload = () => {
+        setImg([...img, reader.result]);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const reader = new FileReader();
+    console.log(img);
+
     const productName = document.getElementById("productName").value;
+    const productPrice = document.getElementById("productPrice").value;
     const productDescription =
       document.getElementById("productDescription").value;
     const franchise = document.getElementById("franchise").value;
@@ -12,20 +32,30 @@ const AddProduct = () => {
     const condition = document.getElementById("condition").value;
     const sizeInCm = document.getElementById("sizeInCm").value;
     const date = document.getElementById("date").value;
-    const image1 = document.getElementById("img1").value;
-    const image2 = document.getElementById("img2").value;
-    const image3 = document.getElementById("img3").value;
 
-    console.log("Product Name:", productName);
-    console.log("Product Description:", productDescription);
-    console.log("Franchise:", franchise);
-    console.log("Product Type:", productType);
-    console.log("Condition:", condition);
-    console.log("Size in cm:", sizeInCm);
-    console.log("Date:", date);
-    console.log("Image 1:", image1);
-    console.log("Image 2:", image2);
-    console.log("Image 3:", image3);
+    const productData = {
+      productName: productName,
+      price: parseFloat(productPrice),
+      description: productDescription,
+      image: img,
+      isFranchise: franchise,
+      type: productType,
+      condition: condition,
+      dimensions: sizeInCm,
+      date: date,
+    };
+
+    console.log(productData);
+
+    fetch("http://localhost:5000/products", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    })
+        .then((res) => res.json())
+      .then((data) => console.log(data));
   };
 
   return (
@@ -50,6 +80,14 @@ const AddProduct = () => {
               id="productDescription"
             />
           </label>
+          <label className="flex flex-col gap-3" htmlFor="productPrice">
+            <h1>Product Price</h1>
+            <input
+              className="px-6 py-3 border rounded-lg"
+              type="number"
+              id="productPrice"
+            />
+          </label>
         </form>
         <h1 className="text-2xl capitalize font-semibold">Category</h1>
         <form className="flex flex-col gap-3 border p-3 rounded " action="">
@@ -58,7 +96,7 @@ const AddProduct = () => {
             <select
               name="franchise"
               id="franchise"
-              className="block px-4  py-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="block px-4  py-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-primary"
             >
               <option value="marvel" className="py-2">
                 Marvel
@@ -79,7 +117,7 @@ const AddProduct = () => {
             <select
               name="productType"
               id="productType"
-              className="block w-full px-4 py-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="block w-full px-4 py-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-primary"
             >
               <option value="" className="py-2">
                 Select Product Type
@@ -104,7 +142,7 @@ const AddProduct = () => {
           <label className="flex flex-col gap-3" htmlFor="condition">
             <h1>New or Used</h1>
             <select
-              className="block w-full px-4 py-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="block w-full px-4 py-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-primary"
               name="condition"
               id="condition"
             >
@@ -124,33 +162,90 @@ const AddProduct = () => {
         <form className="gap-3 border p-3 rounded grid grid-cols-3" action="">
           <label
             htmlFor="img1"
-            className="border-blue-500 p-3 border-dotted border flex items-center align-middle rounded col-span-1 justify-center aspect-square"
+            className="border-blue-500  border-dotted border flex items-center align-middle rounded col-span-1 justify-center aspect-square"
           >
-            <div className="flex flex-col gap-3 justify-center items-center text-center">
-              <CiImageOn className="text-4xl" />
-              <h1>Drop your image or click here to browse</h1>
+            <div className="flex flex-col h-full w-full gap-3 justify-center items-center text-center">
+              {img.length > 0 ? (
+                <img
+                  className="h-full w-full object-cover"
+                  src={img[0]}
+                  alt="Uploaded"
+                />
+              ) : (
+                <>
+                  <CiImageOn className="text-4xl" />
+                  <h1 className="text-sm w-9/12 text-gray-500">
+                    Drop your image or click here to browse
+                  </h1>
+                </>
+              )}
             </div>
-            <input type="file" hidden name="image1" id="img1" />
+            <input
+              type="file"
+              hidden
+              name="image1"
+              id="img1"
+              onChange={handleImgSubmit}
+              accept="image/png"
+            />
           </label>
           <label
             htmlFor="img2"
-            className="border-blue-500 p-3 border-dotted border flex items-center align-middle rounded col-span-1 justify-center aspect-square"
+            className="border-blue-500  border-dotted border flex items-center align-middle rounded col-span-1 justify-center aspect-square"
           >
-            <div className="flex flex-col gap-3 justify-center items-center text-center">
-              <CiImageOn className="text-4xl" />
-              <h1>Drop your image or click here to browse</h1>
+            <div className="flex flex-col gap-3 w-full h-full justify-center items-center text-center">
+              {img.length > 1 ? (
+                <img
+                  className="h-full w-full object-cover"
+                  src={img[1]}
+                  alt="Uploaded"
+                />
+              ) : (
+                <>
+                  <CiImageOn className="text-4xl" />
+                  <h1 className="text-sm w-9/12 text-gray-500">
+                    Drop your image or click here to browse
+                  </h1>
+                </>
+              )}
             </div>
-            <input type="file" hidden name="image2" id="img2" />
+            <input
+              type="file"
+              hidden
+              name="image2"
+              id="img2"
+              onChange={handleImgSubmit}
+              accept="image/png"
+            />
           </label>
           <label
             htmlFor="img3"
-            className="border-blue-500 p-3 border-dotted border flex items-center align-middle rounded col-span-1 justify-center aspect-square"
+            className="border-blue-500  border-dotted border flex items-center align-middle rounded col-span-1 justify-center aspect-square"
           >
-            <div className="flex flex-col gap-3 justify-center items-center text-center">
-              <CiImageOn className="text-4xl" />
-              <h1>Drop your image or click here to browse</h1>
+            <div className="flex flex-col w-full h-full gap-3 justify-center items-center text-center">
+              {img.length > 2 ? (
+                <img
+                  className="w-full h-full object-cover"
+                  src={img[2]}
+                  alt="Uploaded"
+                />
+              ) : (
+                <>
+                  <CiImageOn className="text-4xl" />
+                  <h1 className="text-sm w-9/12 text-gray-500">
+                    Drop your image or click here to browse
+                  </h1>
+                </>
+              )}
             </div>
-            <input type="file" hidden name="image3" id="img3" />
+            <input
+              type="file"
+              hidden
+              name="image3"
+              id="img3"
+              onChange={handleImgSubmit}
+              accept="image/png"
+            />
           </label>
         </form>
         <h1 className="text-2xl font-semibold capitalize">Select Size</h1>
@@ -174,7 +269,7 @@ const AddProduct = () => {
         </form>
         <button
           onClick={handleSubmit}
-          className="py-3 bg-primary"
+          className="py-4 bg-primary "
           type="submit"
         >
           Submit
